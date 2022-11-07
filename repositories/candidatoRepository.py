f4c07b7ed6ddb5c5074091bf74ef4030d8b92242
hoyosjavier56 <78464392+JavierHoyos@users.noreply.github.com>
2022-11-07 17:26:31 -0500

    Creación de repositorio

---
+++ repositories/candidatoRepository.py
@@ -0,0 +1,6 @@
+from repositories.interface_repository import InterfaceRepository
+from models.candidateModel import Candidate
+
+
+class CandidatoRepository(InterfaceRepository[Candidate]):
+    pass
