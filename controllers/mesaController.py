ed103e7d0704dc0a7aa98df751572f699603da27
unknown <abrilari365@gmail.com>
2022-11-08 16:47:27 -0500

    se agregan cambios mesa

--- controllers/mesaController.py
+++ controllers/mesaController.py
@@ -5,13 +5,13 @@ class MesaController:
 
     def __init__(self):
         """
-
+        This is the constructor of MesaController class
         """
         print("Mesa Controller ready")
 
     def index(self) -> list:
         """
-
+        This method returns all 'Mesa' persisted in the DB
         :return:
         """
         data = {
@@ -24,12 +24,12 @@ class MesaController:
 
     def show(self, id_: str) -> list:
         """
-
+        This method returns a 'Mesa' according to the ID requested in the DB
         :param id_:
         :return:
         """
         data = {
-            "_id": "1",
+            "_id": id_,
             "numero_mesa": "1",
             "cantidad_inscritos": "30"
         }
@@ -38,16 +38,17 @@ class MesaController:
 
     def create(self, mesa_: dict) -> dict:
         """
-
+        This method allows entering information of a 'Mesa' to the BD
         :param mesa_:
         :return:
         """
+        print("insert a Mesa")
         mesa = Mesa(mesa_)
         return mesa.__dict__
 
     def update(self, id_: str, mesa_: dict) -> dict:
         """
-
+        This method allows updating information of a 'Mesa' to the BD
         :param id_:
         :param mesa_:
         :return:
@@ -60,7 +61,7 @@ class MesaController:
 
     def delete(self, id_: str) -> dict:
         """
-
+        This method allows you to remove information of a 'Mesa' to the BD
         :param id_:
         :return:
         """
---
+++ repositories/mesaRepository.py
@@ -0,0 +1,6 @@
+from repositories.interface_repository import InterfaceRepository
+from models.votoModel import Voto
+
+
+class MesaRepository (InterfaceRepository[Mesa]):
+    pass
