fcf84be1b1221948292aa92806718daed22fed13
hoyosjavier56 <78464392+JavierHoyos@users.noreply.github.com>
2022-11-08 22:19:03 -0500

    Update candidatoController.py

--- controllers/candidatoController.py
+++ controllers/candidatoController.py
@@ -17,41 +17,48 @@ class CandidateController:
         :return: Candidate's list
         """
         response = CandidatoRepository().find_all()
-
+        print(response)
         return response
-    # Equivalent to 'one by id'
-    def show(self, id_) -> dict:
-        """
 
-         This method returns a 'Candidate' according to the ID requested in the DB
-        :param id_:
-        :return:
+    # Equivalent to 'one by id'
+    def show(self, id_: str) -> list:
         """
-        pass
+         This method returns a 'Candidate' according to the ID requested in the DB
+        :param id_: candidato id str
+        :return: candidato according id
+        """
+        response = CandidatoRepository().find_by_id(id_)
+        candidato = Candidato(response)
+        return candidato.__dict__
+
     # Equivalent to 'insert'
 
-    def create(self, candidate_) -> dict:
+    def create(self, candidate_: dict) -> dict:
         """
          This method allows entering information of a 'Candidate' to the BD
         :param candidate_:
-        :return:
+        :return: candidato created
         """
-        pass
+        candidato = Candidato(candidate_)
+
+        return CandidatoRepository().save(candidato)
 
     def update(self, id_, candidate_) -> dict:
         """
-        
+
         This method allows updating information of a 'Candidate' to the BD
-        :param id_:
+        :param id_: Candidato ID for update
         :param candidate_:
         :return:
         """
-        pass
+        response = CandidatoRepository().update(id_, candidate_)
+        return response
 
-    def delete(self, id_) -> dict:
+    def delete(self, id_: str) -> dict:
         """
          This method allows you to remove information of a 'Candidate' to the BD
         :param id_:
         :return:
         """
-        pass
+        CandidatoRepository().delete(id_)
+        return {"Delete count": 1}
